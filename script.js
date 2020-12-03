2ab21fd322d43c69413aa902eaef3db4eb5994c7
todtsies <emilybtodt@gmail.com>
2020-12-03 15:14:41 -0500

    Update script.js
    Compacted the code that was given to me by adding the event listener to the generate btn. Added my arrays containing all of the useable content in each category

--- script.js
+++ script.js
@@ -1,5 +1,91 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Assignment Code and added event listener to generate button
+var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
+
+// These are my arrays
+let specialChar = ["&", ",", "!", "(", ")", "_", "[", "]", "%", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+let letterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,6 +96,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
