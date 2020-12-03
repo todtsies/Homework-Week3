a4835bc98baa559ca141c603b6649f4e3e60a943
todtsies <emilybtodt@gmail.com>
2020-12-03 15:20:55 -0500

    Variables and first prompt
    Added all of my variables to use later and created my first function to bring up the prompt when the generatePassword btn is clicked

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ let letterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 let letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+// Declaring my variables
+let confirmLength = " ";
+let confirmSpecialChar;
+let confirmNumbers;
+let confirmLowercase;
+let confirmUppercase;
 
+//First prompt to confirm how many characters the user would like
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
 
 
